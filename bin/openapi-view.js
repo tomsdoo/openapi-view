9c086f7e3d5b58b6254b8f86b5a06e78a81a2acb
tomsdoo <toms@live.jp>
2023-09-30 07:56:14 +0900

    commander interface is applied

--- bin/openapi-view.js
+++ bin/openapi-view.js
@@ -6,22 +6,48 @@ const path = require("path");
 const os = require("os");
 const yamljs = require("yamljs");
 const opener = require("opener");
+const program = require("commander");
 
-const argv = process.argv.slice(2);
+const COMMAND_NAME = "openapi-view";
 
-const tempDir = path.join(os.tmpdir(), "openapi-view");
-fs.mkdirSync(tempDir, { recursive: true });
-const indexHtmlFileName = path.join(tempDir, "index.html");
-const defJsFileName = path.join(tempDir, "spec.js");
+program
+  .option("--spec <spec.yml>", "API spec file")
+  .option("--help", "show help")
+  .action(work)
+  .parse();
 
-const indexHtmlContent = fs.readFileSync(path.join(__dirname, "../resources/index.html"), { encoding: "utf8" });
-fs.writeFileSync(indexHtmlFileName, indexHtmlContent);
+function outHelp() {
+  `
+  usage: ${COMMAND_NAME} [option]
 
-const yamlContent = fs.readFileSync(argv[0], { encoding: "utf8" });
-const definitions = yamljs.parse(yamlContent);
+  example:
+  ${COMMAND_NAME} --spec spec.yml
+  ${COMMAND_NAME} --help
+  `.split("\n").forEach(line => {
+    console.log(line);
+  });
+}
 
-const defContent = `window.swaggerSpec = ${JSON.stringify(definitions, null, 2)}`;
-fs.writeFileSync(defJsFileName, defContent);
+function work(options, command) {
+  const specFile = options.spec;
+  if(options.help || !specFile) {
+    outHelp();
+    return;
+  }
 
-console.log(tempDir);
-opener(indexHtmlFileName);
+  const tempDir = path.join(os.tmpdir(), "openapi-view");
+  fs.mkdirSync(tempDir, { recursive: true });
+  const indexHtmlFileName = path.join(tempDir, "index.html");
+  const defJsFileName = path.join(tempDir, "spec.js");
+  
+  const indexHtmlContent = fs.readFileSync(path.join(__dirname, "../resources/index.html"), { encoding: "utf8" });
+  fs.writeFileSync(indexHtmlFileName, indexHtmlContent);
+  
+  const yamlContent = fs.readFileSync(specFile, { encoding: "utf8" });
+  const definitions = yamljs.parse(yamlContent);
+  
+  const defContent = `window.swaggerSpec = ${JSON.stringify(definitions, null, 2)}`;
+  fs.writeFileSync(defJsFileName, defContent);
+  
+  opener(indexHtmlFileName);
+}
